566594a23c203c8b147959c376f1c24b7596e3c2
ChongChong He <he.cc.phy@gmail.com>
2020-01-24 17:49:56 -0500

    first commit

---
+++ mytemplates/my-python-template.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" `(insert-filename)`
+
+Attributes
+----------
+classes and functions defined
+
+Example
+-------
+
+Notes
+-----
+
+"""
+
+#-----------------------------------------------------------------------------
+#    Author: ChongChong He
+#    Data: Jun 21, 2018
+#-----------------------------------------------------------------------------
+
+from __future__ import division, print_function
+import numpy as np
+import matplotlib.pyplot as plt
+
